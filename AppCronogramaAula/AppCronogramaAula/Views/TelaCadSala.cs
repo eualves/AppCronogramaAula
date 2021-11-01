967ed1ec59e16043b867fe06155d5d08b36c6b31
Leo Alves <78911425+leobico@users.noreply.github.com>
2021-11-01 19:11:06 -0300

    Add files via upload

---
+++ AppCronogramaAula/AppCronogramaAula/Views/TelaCadSala.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using AppCronogramaAula.Model;
+using AppCronogramaAula.Controller;
+
+namespace CronogramaAula
+{
+    public partial class TelaCadSala : Form
+    {
+        public TelaCadSala()
+        {
+            InitializeComponent();
+        }
+        private void btnCadSala_Click(object sender, EventArgs e)
+        {
+            Sala.NomeSala = tbNomeSala.Text;
+            Sala.TipoSala= label3.Text; //mudar tbTipoSala
+
+
+            SalaController salaController = new SalaController();
+            salaController.cadastroSala();
+
+            tbNomeSala.Clear();
+            label3.Clear(); // ?
+
+
+            if (Sala.Retorno == "True")
+            {
+                this.Close();
+            }
+        }
+    }
+}
+
